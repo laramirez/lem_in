07efbb47e7ac967c4f9aea0289e019ac5010e401
laramirez <ramirez_li@hotmail.co.uk>
2017-10-02 11:01:58 -0700

    start on DFS algorithm

---
+++ lem_in_dir/sources/find_all_paths.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   find_all_paths.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lararamirez <lararamirez@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/01 17:47:02 by lararamirez       #+#    #+#             */
+/*   Updated: 2017/10/01 17:47:02 by lararamirez      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/lem_in.h"
+
+char        **get_all_paths_and_cost(t_master *lem_in)
+{
+    char    *visited;
+
+    visited = ft_create_padding(O, lem_in->room_count);
+    
+
+    
+}
