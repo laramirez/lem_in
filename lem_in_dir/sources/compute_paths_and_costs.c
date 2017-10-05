322493e747abf8f82b91944a7e47140f51a6ef23
laramirez <ramirez_li@hotmail.co.uk>
2017-10-04 19:32:51 -0700

    progress on backtracking

--- lem_in_dir/sources/compute_paths_and_costs.c
+++ lem_in_dir/sources/compute_paths_and_costs.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   compute_paths_and_costs.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lramirez <lramirez@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lararamirez <lararamirez@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/04 11:01:26 by lramirez          #+#    #+#             */
-/*   Updated: 2017/10/04 14:12:50 by lramirez         ###   ########.fr       */
+/*   Updated: 2017/10/04 17:52:40 by lararamirez      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,11 +116,61 @@ char		valid_path(t_list *path, size_t end_index)
 	return (0);
 }
 
-void		backtrack_to_next_in_queue(t_list *path, t_stack **queue);
+void		reset_visited(char **visited)
+{
+	size_t	i;
 
+	i = 0;
+	while ((*visited)[i])
+	{
+		(*visited)[i] = '0';
+		i++;
+	}
+}
 
+void		delete_lst(t_list **path)
+{
+	t_list	*tmp;
+	t_list	*tmp_next;
+	
+	tmp = *path;
+	while (tmp)
+	{
+		tmp_next = tmp->next;
+		free(tmp);
+		tmp = tmp_next;
+	}
+	*path = NULL;
+}
 
+size_t		backtrack_to_queued(t_list **path, t_stack **queue,
+				char **visited, size_t end_index)
+{
+	t_list	*new_path;
+	t_list	*tmp;
+	size_t	target_node;
+	size_t	count;
 
+	new_path = NULL;
+	count = 3;
+	reset_visited(visited);
+	tmp = *path;
+	target_node = (*queue)->previous_room;
+	while (*(size_t *)tmp->data != target_node && count)
+	{
+		ft_lstaddend(&new_path, ft_lstnew((size_t *)tmp->data, sizeof(size_t)));
+		(*visited)[*(size_t *)tmp->data] = '1';
+		tmp = tmp->next;
+		count--;
+	}
+	ft_lstaddend(&new_path, ft_lstnew((size_t *)tmp->data, sizeof(size_t)));
+	(*visited)[*(size_t *)tmp->data] = '1';
+	(*visited)[*(size_t *)tmp->next->data] = '1';
+	if (!valid_path(*path, end_index))
+		delete_lst(path);
+	*path = new_path;
+	return (*(size_t *)tmp->data);
+}
 
 void		compute_paths_and_costs(t_master *lem_in)
 {
@@ -128,27 +178,29 @@ void		compute_paths_and_costs(t_master *lem_in)
 	t_list	*path;
 	size_t  current_node;
 	char    *visited;
+	char	start;
 
 	queue = NULL;
 	path = NULL;
+	start = 1;
 	current_node = lem_in->start_index;
 	visited = ft_create_padding('0', lem_in->room_count);
-	while (add_adj_nodes_to_queue(lem_in, current_node, visited, &queue))
+	while (start || queue)
 	{
+		start = 0;
+		while (add_adj_nodes_to_queue(lem_in, current_node, visited, &queue))
+		{
+			ft_lstaddend(&path, ft_lstnew(&current_node, sizeof(size_t)));
+			current_node = pop_off_queue(&queue);
+			printf("current_node is [%zu]\n", current_node);
+			printf("visited (%s)\n", visited);
+		}
 		ft_lstaddend(&path, ft_lstnew(&current_node, sizeof(size_t)));
-		current_node = pop_off_queue(&queue);
+		if (valid_path(path, lem_in->end_index))
+			add_to_path_lst(lem_in, &path);
+		print_queue(&queue);
+		current_node = backtrack_to_queued(&path, &queue, &visited, lem_in->end_index);
 	}
-	ft_lstaddend(&path, ft_lstnew(&current_node, sizeof(size_t)));
-	if (valid_path(path, lem_in->end_index))
-		add_to_path_lst(lem_in, &path);
-	backtrack_to_next_in_queue(path, &queue)
-
-	// print_queue(&queue);
-	// while ( )
-	// {
-	// 	get_path(lem_in, lem_in->start_index, &visited, &i_divert);
-	// 	display_paths(lem_in);
-	// }
+	display_paths(lem_in);
 	// free(visited);
-	display_paths(lem_in);	
 }
